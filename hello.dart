4fb2b100c29c4ad8dd37d48a857f7e45081c502c
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-09-01 00:43:38 +0530

    Update hello.dart

--- hello.dart
+++ hello.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 // center is widget that align elements in center
 // home is is must 
 //main part is the starting point of the page
+// child is inside node like of center
 void main() {
   runApp(
     const MaterialApp(
